1ec1cc5d7c6f31fdff3cbd1304ab591898d791ee
Gian Ott <nichtgian@gmail.com>
2024-03-13 20:43:10 +0100

    SW4 Resume counter

--- src/sw3/app/src/main/java/ch/hslu/sw3/MainActivity.kt
+++ src/sw3/app/src/main/java/ch/hslu/sw3/MainActivity.kt
@@ -1,16 +1,15 @@
 package ch.hslu.sw3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout_fragmentContainer, OverviewFragment())
+            .commit()
     }
 }
--- src/sw3/app/src/main/java/ch/hslu/sw3/OverviewFragment.kt
+++ src/sw3/app/src/main/java/ch/hslu/sw3/OverviewFragment.kt
@@ -1,7 +1,44 @@
 package ch.hslu.sw3
 
+import android.content.Context.MODE_PRIVATE
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ch.hslu.sw3.databinding.FragmentOverviewBinding
 
 class OverviewFragment : Fragment(R.layout.fragment_overview) {
+    private var _binding: FragmentOverviewBinding? = null
+    private val binding get() = _binding!!
 
+    private val COUNTER_KEY = "COUNTER"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val preferences = requireActivity().getSharedPreferences(
+            getString(R.string.preference_file_key),
+            MODE_PRIVATE)
+        val counter = preferences.getInt(COUNTER_KEY, 0) + 1
+
+        preferences.edit()
+            .putInt(COUNTER_KEY, counter)
+            .apply()
+
+        binding.textViewCounter.text = "MainActivity onResume wurde $counter mal aufgerufen."
+    }
 }
